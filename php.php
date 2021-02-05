6b224731e82abb6b29d1f58e58ba0850180d3bcb
VictorCaraballo <78553233+VictorCaraballo@users.noreply.github.com>
2021-02-04 21:31:51 -0300

    Update php.php

--- php.php
+++ php.php
@@ -1,13 +1,22 @@
-<?php
-//En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
- $destino = "victorcaraballocontacto@gmail.com";
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $email = $_POST["email"];
-    $numero = $_POST["numero"];
-    $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
-    mail($destino, "Contacto", $contenido);
-    $header("Location: index.html");
-//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "victorcaraballocontacto@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
+
 ?>
