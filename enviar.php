6c2adb5adc85f4c1039588635968ab9d80b67993
VictorCaraballo <78553233+VictorCaraballo@users.noreply.github.com>
2021-02-04 21:53:47 -0300

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<?php
+//En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
+ $destino = "victorcaraballocontacto@gmail.com";
+
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $email = $_POST["email"];
+    $numero = $_POST["numero"];
+    $mensaje = $_POST["mensaje"];
+
+    $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+
+    $header("Location: index.html");
+//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+?>
